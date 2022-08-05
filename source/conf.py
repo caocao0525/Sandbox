dc7da91ea3d15571657d4d6a64ffdc37ba258d0e
CaoCao0525 <70701644+caocao0525@users.noreply.github.com>
2022-08-05 17:26:01 +0900

    Upload source
    test

---
+++ source/conf.py
@@ -0,0 +1,64 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Path setup --------------------------------------------------------------
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
+# import os
+# import sys
+# sys.path.insert(0, os.path.abspath('.'))
+
+# -- Style -------------------------------------------------------------------
+from pygments.styles import get_all_styles
+styles=list(get_all_styles())
+
+# -- Project information -----------------------------------------------------
+
+project = 'Research Note by Schona'
+copyright = '2022, Seohyun Lee'
+author = 'Seohyun Lee'
+
+
+# -- General configuration ---------------------------------------------------
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+extensions = [
+]
+
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ['_templates']
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = []
+
+
+# -- Options for HTML output -------------------------------------------------
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+html_theme = 'sphinx_rtd_theme'
+
+# -- Github connection -------------------------------------------------------
+html_context = {
+    "display_github": True, # Integrate GitHub
+    "github_user": "CaoCao0525", # Username
+    "github_repo": "Sandbox", # Repo name
+    "github_version": "master", # Version
+    "conf_py_path": "/source/", # Path in the checkout to the docs root
+}
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ['_static']
---
+++ source/index.rst
@@ -0,0 +1,35 @@
+.. test_sphinx documentation master file, created by
+   sphinx-quickstart on Thu Aug  4 10:38:17 2022.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+=======================================
+Research Note
+=======================================
+
+Moved from Slack, new notes here is to record every single details I am encountering during my research.
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+Other topics
+=============
+
+   This is a simple example:
+
+.. code-block:: python
+
+       import math
+       print 'import done'
+
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
